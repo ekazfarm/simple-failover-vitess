e71b280094445860a2992dbf6fe79f1caefeb79a
ekazfarm <syachferdian4@gmail.com>
2022-12-08 11:32:26 +0700

    script reparent vitess v1

---
+++ reparent-vitess-node.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Set target host master IP 
+TARGET_HOST='192.168.4.100'
+
+
+count=$(ping -c 3 $TARGET_HOST | grep from* | wc -l)
+
+if [ $count -eq 0 ]; then
+    echo "$(date)" "Target host" $TARGET_HOST "unreachable, Rebooting!" >>/var/log/dbrep.log
+    /usr/local/vitess/bin/vtctlclient --server $localhost:15999 TabletExternallyReparented zone1-410
+
+
+else
+    /usr/local/vitess/bin/vtctlclient --server $localhost:15999 TabletExternallyReparented zone1-409
+    echo "$(date) ===-> Back To Master Node Success " >>/var/log/dbrep.log
+fi
